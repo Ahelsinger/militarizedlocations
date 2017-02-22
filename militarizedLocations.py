79d8769c3bba9925a62dfcb97261680c7eedf9ea
Adam Helsinger <Ahelsinger@users.noreply.github.com>
2017-02-22 12:51:25 -0600

    Update militarizedLocations.py

--- militarizedLocations.py
+++ militarizedLocations.py
@@ -9,16 +9,16 @@ templengthofList = 100
 
 # PRINTING THE DIFFERENT CELLS
 # print df.head()
-print df.iloc[0][0],  df.iloc[0][1],  df.iloc[0][2], df.iloc[0][3], df.iloc[0][4]
-print
-print "df.iloc[0][5]"
-print df.iloc[0][5]
-print
-print "df.iloc[0][6]"
-print df.iloc[0][6]
-print "LatLon?"
-print df.iloc[0][7]
-print df.iloc[0][8]
+# print df.iloc[0][0],  df.iloc[0][1],  df.iloc[0][2], df.iloc[0][3], df.iloc[0][4]
+# print
+# print "df.iloc[0][5]"
+# print df.iloc[0][5]
+# print
+# print "df.iloc[0][6]"
+# print df.iloc[0][6]
+# print "LatLon"
+# print df.iloc[0][7]
+# print df.iloc[0][8]
 
 print
 print
@@ -26,11 +26,6 @@ print
 # print np.where(pd.isnull(df))
 
 nanPlaces = np.where(pd.isnull(df))
-
-#printing the NaN cells
-# for i in range(templengthofList):
-# 	if nanPlaces:
-# 		print nameLatLon.iloc[i][0]
 
 import folium 
 map=folium.Map(location=[50.060467, 14.441584],zoom_start=3,tiles='Stamen Terrain')
@@ -40,8 +35,7 @@ for i in range(templengthofList):
 	theLon = df.iloc[i][8]
 	map.add_child(folium.Marker(location=[(theLat),(theLon)],popup=(theName),icon=folium.Icon(color="blue")))
 map.save(outfile='militarizedlocations.html')
-# was Makermap0.html
 
 # print dir(map.save)
-print
 # print help(map.save)
+print
